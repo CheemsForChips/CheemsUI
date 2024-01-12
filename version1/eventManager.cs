a2da5444afd7ac8070ccb55537537bb7daeff6ba
CheemsForChips <128451445+CheemsForChips@users.noreply.github.com>
2024-01-13 01:56:09 +0800

    WPF版本已更新

---
+++ version1/eventManager.cs
@@ -0,0 +1,40 @@
+public class EventManager{
+    //单例模式 现阶段觉得没什么用 不需要单例模式
+    private static EventManager eventManagerInstance=new EventManager();
+    private EventManager(){
+        //构造函数
+        Console.WriteLine("EventManagerInstance is created");
+    }
+    public static EventManager GetInstance(){
+        if(eventManagerInstance == null){
+            eventManagerInstance = new EventManager();
+        }
+        return eventManagerInstance;
+    }
+    #region 传入选择的区块编号的委托
+    public delegate void SelectedToString(int index,System.Drawing.Size clientSize);
+    public SelectedToString CheemsStringDelegate  = null;
+    public static void showSelectedAction(int index,System.Drawing.Size clientSize){
+        // Label textLabel = new Label();
+        // textLabel.Text = index.ToString();
+        // textLabel.Font = new Font("微软雅黑", 20, FontStyle.Bold);
+
+        // textLabel.AutoSize = true;
+        // textLabel.Location = new Point(clientSize.Width / 2, clientSize.Height / 2);
+        // textLabel.BackColor = Color.Transparent;
+        // textLabel.ForeColor = Color.White;
+        // textLabel.Parent = RoundForm.GetInstance();
+        // textLabel.BringToFront();
+        // textLabel.Show();
+        
+    }
+    #endregion
+
+    #region 退出程序的委托
+    public delegate void ExitApplication();
+    public ExitApplication CheemsExitApplicationDelegate = null;
+    public static void applicationExit(){
+        Application.Exit();
+    }
+    #endregion
+}
---
+++ version3/CircleControl/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System.Configuration;
+using System.Text;
+using System.Drawing.Drawing2D;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CircleControl
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static int SectorSegement;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Button button = sender as Button;
+            //UtilityTextShow.Text = button.Name;
+        }
+
+        private void Button_MouseLeave(object sender, MouseEventArgs e)
+        {
+            //UtilityTextShow.Text = "CheemsShow";
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+
+            Point center = new Point(ActualWidth / 2, ActualHeight / 2);
+            double dx = e.GetPosition(this).X - center.X;
+            double dy = e.GetPosition(this).Y - center.Y;
+            double angle = (Math.Atan2(dy, dx) * 180 / Math.PI + 360) % 360;
+
+            int newSegment = (int)(angle / 45);
+            {
+                SectorSegement = newSegment;
+                UtilityTextShow.Text = newSegment.ToString();
+
+            }
+        }
+        protected override void OnMouseDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseDown(e);
+            switch (SectorSegement)
+            {
+                case 0:
+                    Arc0.SectorFill=new SolidColorBrush(Color.FromArgb(0xE0, 0xDF, 0xC0, 0xFF));
+                    break;
+                case 1:
+                    Arc1.SectorFill= new SolidColorBrush(Color.FromArgb(0xE0, 0xDF, 0xC0, 0xFF));
+                    break;
+                case 2:
+                    Arc0.SectorFill = new SolidColorBrush(Color.FromArgb(0xE0, 0xDF, 0xC0, 0xFF));
+                    break;
+                case 3:
+                    Arc1.SectorFill = new SolidColorBrush(Color.FromArgb(0xE0, 0xDF, 0xC0, 0xFF));
+                    break;
+                case 4:
+                    Arc0.SectorFill = new SolidColorBrush(Color.FromArgb(0xE0, 0xDF, 0xC0, 0xFF));
+                    break;
+                case 5:
+                    Arc1.SectorFill = new SolidColorBrush(Color.FromArgb(0xE0, 0xDF, 0xC0, 0xFF));
+                    break;
+                case 6:
+                    Arc0.SectorFill = new SolidColorBrush(Color.FromArgb(0xE0, 0xDF, 0xC0, 0xFF));
+                    break;
+                case 7:
+                    Arc1.SectorFill = new SolidColorBrush(Color.FromArgb(0xE0, 0xDF, 0xC0, 0xFF));
+                    break;
+            }
+        }
+
+    }
+}
