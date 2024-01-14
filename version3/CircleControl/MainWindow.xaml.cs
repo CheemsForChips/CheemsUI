887231898da96e96102b6c7694cf67e0d5e31363
CheemsForChips <128451445+CheemsForChips@users.noreply.github.com>
2024-01-14 22:22:57 +0800

    🥶已经在功能与视觉效果上超过winform了

--- version3/CircleControl/MainWindow.xaml.cs
+++ version3/CircleControl/MainWindow.xaml.cs
@@ -19,23 +19,29 @@ namespace CircleControl
     public partial class MainWindow : Window
     {
         public static int SectorSegement;
+        // a r g b先是透明通道 然后才是rgb注意区分
+        public static SolidColorBrush[] UnselectedSectorColor = { new SolidColorBrush(Color.FromArgb(0xFF,0x1E, 0x96, 0xFC)),
+                                                                    new SolidColorBrush(Color.FromArgb(0xFF, 0x60, 0xB6, 0xFB )),
+                                                                    new SolidColorBrush(Color.FromArgb(0xFF, 0xA2, 0xD6, 0xF9)),
+                                                                    new SolidColorBrush(Color.FromArgb(0xFF, 0xCF, 0xE5, 0x7D)),
+                                                                    new SolidColorBrush(Color.FromArgb(0xFF, 0xE6, 0xEC, 0x3F)),
+                                                                     new SolidColorBrush(Color.FromArgb(0xFF, 0xFC, 0xF3, 0x00)),
+                                                                    new SolidColorBrush(Color.FromArgb(0xFF, 0x0D, 0x45, 0xD5)),
+                                                                    new SolidColorBrush(Color.FromArgb(0xFF, 0x16, 0x6E, 0xE9))
+                                                                    };
+        public static SolidColorBrush SelectedSectorColor= new SolidColorBrush(Color.FromArgb(0xFF, 0xE0, 0xDF, 0xC0));
+
 
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
         }
 
-        private void Button_MouseEnter(object sender, MouseEventArgs e)
-        {
-            Button button = sender as Button;
-            //UtilityTextShow.Text = button.Name;
-        }
-
-        private void Button_MouseLeave(object sender, MouseEventArgs e)
-        {
-            //UtilityTextShow.Text = "CheemsShow";
-        }
-
+        /// <summary>
+        /// 旋转鼠标标记控件
+        /// </summary>
+        /// <param name="e"></param>
         protected override void OnMouseMove(MouseEventArgs e)
         {
             base.OnMouseMove(e);
@@ -49,39 +55,57 @@ namespace CircleControl
             {
                 SectorSegement = newSegment;
                 UtilityTextShow.Text = newSegment.ToString();
+            }
 
+            for(int i = 0; i < 8; i++)
+            {
+                bool isSelect = (i == SectorSegement);
+                SolidColorBrush solidColorBrush = isSelect ? SelectedSectorColor : UnselectedSectorColor[i];
+                switch (i)
+                {
+                    case 0:Arc0.SectorFill = solidColorBrush; break;
+                    case 1: Arc1.SectorFill = solidColorBrush;break;
+                    case 2: Arc2.SectorFill = solidColorBrush;break;
+                    case 3: Arc3.SectorFill = solidColorBrush;break;
+                    case 4: Arc4.SectorFill = solidColorBrush;break;
+                    case 5: Arc5.SectorFill = solidColorBrush;break;
+                    case 6: Arc6.SectorFill = solidColorBrush;break;
+                    case 7 : Arc7.SectorFill = solidColorBrush;break;
+                }
             }
         }
+
+
+        /// <summary>
+        /// 点击鼠标 触发控件的事件
+        /// </summary>
+        /// <param name="e"></param>
         protected override void OnMouseDown(MouseButtonEventArgs e)
         {
             base.OnMouseDown(e);
-            switch (SectorSegement)
+
+            switch(SectorSegement)
             {
                 case 0:
-                    Arc0.SectorFill=new SolidColorBrush(Color.FromArgb(0xE0, 0xDF, 0xC0, 0xFF));
+                    UtilityModel.GetUtilityModelInstance().ExecuteCommand("taskmgr");
                     break;
                 case 1:
-                    Arc1.SectorFill= new SolidColorBrush(Color.FromArgb(0xE0, 0xDF, 0xC0, 0xFF));
                     break;
                 case 2:
-                    Arc0.SectorFill = new SolidColorBrush(Color.FromArgb(0xE0, 0xDF, 0xC0, 0xFF));
                     break;
                 case 3:
-                    Arc1.SectorFill = new SolidColorBrush(Color.FromArgb(0xE0, 0xDF, 0xC0, 0xFF));
                     break;
                 case 4:
-                    Arc0.SectorFill = new SolidColorBrush(Color.FromArgb(0xE0, 0xDF, 0xC0, 0xFF));
                     break;
                 case 5:
-                    Arc1.SectorFill = new SolidColorBrush(Color.FromArgb(0xE0, 0xDF, 0xC0, 0xFF));
+                    UtilityModel.GetUtilityModelInstance().ExitCircleControl();
                     break;
                 case 6:
-                    Arc0.SectorFill = new SolidColorBrush(Color.FromArgb(0xE0, 0xDF, 0xC0, 0xFF));
                     break;
                 case 7:
-                    Arc1.SectorFill = new SolidColorBrush(Color.FromArgb(0xE0, 0xDF, 0xC0, 0xFF));
                     break;
             }
+
         }
 
     }
---
+++ version3/CircleControl/UtilityModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CircleControl
+{
+    class UtilityModel
+    {
+        /// <summary>
+        /// 单例 确保仅有一个功能库
+        /// </summary>
+        private UtilityModel() { }
+        private static UtilityModel UtilityModelInstance=new UtilityModel();
+        public static UtilityModel GetUtilityModelInstance()
+        {
+            if (UtilityModelInstance==null)
+            {
+                UtilityModelInstance=new UtilityModel();
+            }
+            return UtilityModelInstance;
+        }
+
+
+
+        /// <summary>
+        /// 功能函数定义
+        /// </summary>
+        public void ExitCircleControl()
+        {
+            Application.Current.Shutdown();
+        }
+
+
+        public  void ExecuteCommand(string command)
+        {
+            var processInfo = new ProcessStartInfo()
+            {
+                FileName = "cmd.exe",
+                Arguments = $"/C {command}",
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardOutput = true
+            };
+
+            using (var process = new Process())
+            {
+                process.StartInfo = processInfo;
+                process.OutputDataReceived += (sender, e) => Console.WriteLine(e.Data);
+                process.Start();
+                process.BeginOutputReadLine();
+                process.WaitForExit();
+            }
+        }
+    }
+}
